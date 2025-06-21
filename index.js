2177c1832ce1b8e1019dc155fe23fe09f04182d8
MDixey17 <76823860+MDixey17@users.noreply.github.com>
2025-06-20 20:02:51 -0500

    fix: Offset Cron for Render (#4)

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const env = require("dotenv").config();
 
 // Cron: Every day at 4am
 // 0 1 * * *
-cron.schedule("0 4 * * *", () => {
+cron.schedule("0 7 * * *", () => {
     getRecentEbaySales();
 });
 
 // Cron: Every day at midnight
 // 0 0 * * *
-cron.schedule("0 0 * * *", () => {
+cron.schedule("0 5 * * *", () => {
     createDailyCardBattle();
 })
